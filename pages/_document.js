7a6a4ef97c56d16656a62a0f43c06a0a1900fb6a
Milind Palaria <lit2021047@iiitl.ac.in>
2023-04-20 17:28:35 +0530

    Off Canvas

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,7 @@ export default function Document() {
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
         crossorigin="anonymous"
       />
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
       <body>
         <Main />
         <NextScript />
