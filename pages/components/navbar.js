65aa2af4c1855f2ea2879e9bf81892133d084311
Harsh Kanojia <96467990+Eyesnatcher-coder@users.noreply.github.com>
2023-04-22 14:50:33 +0530

    getting changes

--- pages/components/navbar.js
+++ pages/components/navbar.js
@@ -17,125 +17,106 @@ function Navbar1() {
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+
   return (
     <>
       {["lg"].map((expand) => (
-        <Navbar key={expand}  expand={expand} className="mb-3 navbar">
-          <Container fluid>
-            <div className="left-nav">
-              <Button
-                variant="primary"
-                onClick={handleShow}
-                className="me-2 nav-btn"
-              >
-                <span class="material-symbols-outlined">menu</span>
-              </Button>
-              <h1 style={{fontSize:"1.5rem",fontWeight:"bold"}} className={play.className}>ShauKeens</h1>
-            </div>
-            <div>
-              <Button className="me-2 nav-btn sign-in-btn">
-                <span class="material-symbols-outlined">person</span>
-                <span id="signin">Login</span>
-              </Button>
-            </div>
-            <Offcanvas show={show} onHide={handleClose}>
-              <Offcanvas.Header>
-                <Offcanvas.Title className={play.className}>
-                  Shaukeens
-                </Offcanvas.Title>
+        <div>
+          <Navbar key={expand} expand={expand} className="mb-3 navbar">
+            <Container fluid>
+              <div className="left-nav">
                 <Button
                   variant="primary"
-                  onClick={handleClose}
-                  className="me-2 nav-close-btn"
+                  onClick={handleShow}
+                  className="me-2 nav-btn"
                 >
-                  <span class="material-symbols-outlined">close</span>
+                  <span class="material-symbols-outlined">menu</span>
                 </Button>
-              </Offcanvas.Header>
-              <Offcanvas.Body>
-                <hr />
-                <div className="btn-div">
-                  <div>
-                    <span class="material-symbols-outlined">
-                      account_circle
-                    </span>
-                    <Button>Sign in / Register</Button>
+                <h1 style={{ fontSize: "1.5rem", fontWeight: "bold" }} className={play.className}>ShauKeens</h1>
+              </div>
+              <div>
+                <Button className="me-2 nav-btn sign-in-btn">
+                  <span class="material-symbols-outlined">person</span>
+                  <span id="signin">Login</span>
+                </Button>
+              </div>
+              <Offcanvas show={show} onHide={handleClose}>
+                <Offcanvas.Header>
+                  <Offcanvas.Title className={play.className}>
+                    Shaukeens
+                  </Offcanvas.Title>
+                  <Button
+                    variant="primary"
+                    onClick={handleClose}
+                    className="me-2 nav-close-btn"
+                  >
+                    <span class="material-symbols-outlined">close</span>
+                  </Button>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                  <hr />
+                  <div className="btn-div">
+                    <div>
+                      <span class="material-symbols-outlined">
+                        account_circle
+                      </span>
+                      <Button>Sign in / Register</Button>
+                    </div>
+                    <div>
+                      <span class="material-symbols-outlined">recommend</span>
+                      <Button>Just For You</Button>
+                    </div>
                   </div>
-                  <div>
-                    <span class="material-symbols-outlined">recommend</span>
-                    <Button>Just For You</Button>
+                  <hr />
+                  <div className="btn-div">
+                    <div>
+                      <span class="material-symbols-outlined">
+                        directions_boat
+                      </span>
+                      <Button>Yachts</Button>
+                    </div>
+                    <div>
+                      <span class="material-symbols-outlined">villa</span>
+                      <Button>Real Estates</Button>
+                    </div>
+                    <div>
+                      <span class="material-symbols-outlined">helicopter</span>
+                      <Button>Helicopters</Button>
+                    </div>
+                    <div>
+                      <span class="material-symbols-outlined">
+                        flight_takeoff
+                      </span>
+                      <Button>Jets</Button>
+                    </div>
+                    <div>
+                      <span class="material-symbols-outlined">two_wheeler</span>
+                      <Button>Bikes</Button>
+                    </div>
+                    <div>
+                      <span class="material-symbols-outlined">
+                        directions_car
+                      </span>
+                      <Button>Cars</Button>
+                    </div>
                   </div>
-                </div>
-                <hr />
-                <div className="btn-div">
-                  <div>
-                    <span class="material-symbols-outlined">
-                      directions_boat
-                    </span>
-                    <Button>Yachts</Button>
+                  <hr />
+                  <div className="btn-div-2">
+                    <Button>List With Us</Button>
+                    <Button>Help & FAQ</Button>
+                    <Button>About</Button>
+                    <Button>Contact</Button>
                   </div>
-                  <div>
-                    <span class="material-symbols-outlined">villa</span>
-                    <Button>Real Estates</Button>
-                  </div>
-                  <div>
-                    <span class="material-symbols-outlined">helicopter</span>
-                    <Button>Helicopters</Button>
-                  </div>
-                  <div>
-                    <span class="material-symbols-outlined">
-                      flight_takeoff
-                    </span>
-                    <Button>Jets</Button>
-                  </div>
-                  <div>
-                    <span class="material-symbols-outlined">two_wheeler</span>
-                    <Button>Bikes</Button>
-                  </div>
-                  <div>
-                    <span class="material-symbols-outlined">
-                      directions_car
-                    </span>
-                    <Button>Cars</Button>
-                  </div>
-                </div>
-                <hr />
-                <div className="btn-div-2">
-                  <Button>List With Us</Button>
-                  <Button>Help & FAQ</Button>
-                  <Button>About</Button>
-                  <Button>Contact</Button>
-                </div>
-                <hr />
-              </Offcanvas.Body>
-            </Offcanvas>
-            {/* <Navbar.Offcanvas
-              id={`offcanvasNavbar-expand-${expand}`}
-              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-              placement="end"
-            >
-              <Offcanvas.Header closeButton>
-                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                  Offcanvas
-                </Offcanvas.Title>
-              </Offcanvas.Header>
-              <Offcanvas.Body>
-                <Nav className="justify-content-end flex-grow-1 pe-3">
-                  <Nav.Link href="#action1">Home</Nav.Link>
-                  <Nav.Link href="#action2">Link</Nav.Link>
-                </Nav>
-                <Form className="d-flex">
-                  <Form.Control
-                    type="search"
-                    placeholder="Search"
-                    className="me-2"
-                    aria-label="Search"
-                  />
-                  <Button varia.nt="outline-success">Search</Button>
-                </Form>
-              </Offcanvas.Body>
-            </Navbar.Offcanvas> */}
-          </Container>
-        </Navbar>
+                  <hr />
+                </Offcanvas.Body>
+              </Offcanvas>
+
+            </Container>
+
+          </Navbar>
+         
+        </div>
       ))}
     </>
   );
