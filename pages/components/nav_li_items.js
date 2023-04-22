1308d0846a2a8c217e539c2748ea760cb0a3853a
Harsh Kanojia <96467990+Eyesnatcher-coder@users.noreply.github.com>
2023-04-22 20:02:04 +0530

    dynamic nav2

---
+++ pages/components/nav_li_items.js
@@ -0,0 +1,40 @@
+export default function Nav_li_items({ props,item}) {
+    const item_val= item;
+    return (
+        <div onMouseLeave={() => { console.log(document.getElementsByClassName("li_1_div")[0].style.visibility = "hidden") }}
+            className="bg-white -mt-4 pl-10 pt-4 li_1_div ">
+            <div className="flex">
+                {props.map((items) => {
+                    return( 
+                    <div className="flex flex-wrap  ">
+                        {items.map((item)=>{
+                        return<>
+                        {/* <h6 classN  ame="ml-8">{item}</h6> */}
+                        <div className="li_1">
+                            <div>
+                                <ul>
+                                    {item.map((ite) => {
+                                        return <li>{ite}</li>
+                                    })}
+                                </ul>
+                            </div>
+                        </div>
+                        </>
+                        })}
+                        <div className=" ml-5 vertical_bar"></div>
+                    </div> )}   
+                    )
+                }          
+            </div>
+            <div className="flex flex-end justify-end text-center">
+                <button className="bg-black p-2 text-neutral-300 ">View all {item_val}
+
+                </button>
+                <div className="bg-black btn-top"><svg xmlns="http://www.w3.org/2000/svg" height="18"
+                    viewBox="0 96 960 960" fill="white" width="48">
+                    <path d="m561 814-43-42 168-168H160v-60h526L517 375l43-42 241 241-240 240Z" />
+                </svg></div>
+            </div>
+        </div>
+            );
+}
--- pages/components/navbar.js
+++ pages/components/navbar.js
@@ -2,8 +2,9 @@ import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Offcanvas from "react-bootstrap/Offcanvas";
-import {useEffect, useState } from "react";
-import { Unica_One, Quicksand,Bebas_Neue } from "next/font/google";
+import { useEffect, useState } from "react";
+import { Unica_One, Quicksand, Bebas_Neue } from "next/font/google";
+import Nav_li_items from "./nav_li_items";
 
 
 const play = Bebas_Neue({
@@ -15,18 +16,49 @@ const play = Bebas_Neue({
 function Navbar1() {
   const [show, setShow] = useState(false);
 
-  const [color,setColor] =useState(false);
+  const [color, setColor] = useState(false);
+
+  const li_default = [[[],],]
+
+  const [default_item, setdefaultItem] = useState("");
+
+  const li1_items = [[["Popular Builders"], 
+                      ["Sunseeker","Azimut","Riva","Benetti","Custom","Cranchi","Beneteau","Ferretti"],
+                      ["Sea Ray","Sunreef","Chris Craft","Heesen","Pershing","Hatteras","Princess","Viking"]],]
+  const li2_items = [[["Top Countries"], 
+                      ["United States","Spain","Italy","France","Portugal","Canada","United Kingdom","Greece"],
+                      ["Switzerland","United Arab Emirates","Mexico","South Africa","Australia","Germany","Netherlands","Japan"]],
+
+                      [["Top Cities"], 
+                      ["Los Angeles","Marbella","London","New York","Paris","Miami","Ibiza","Beverly Hills"],
+                      ["Cannes","Mallorca","Madrid","Lisbon","Rome","Milan","Nice","Toronto"]],
+
+                      [["Top Regions"],
+                      ["French Riviera","Costa del Sol","French and Swiss Alps","Costa Blanca","California, USA","Hamptons, NY, USA","Florida, USA","Tuscany"],
+                      ["Algarve","Greek Islands","Balearic Islands","Caribbean","Dubai","Amalfi Coast","Lombardy","Costa Brava"]],
+                    ]
+  const li6_items =[[["Popular Makes"],
+                      ["Bugatti","Pagani","Koenigsegg","Ferrari","Lamborghini","Mercedes","Rolls-Royce","McLaren"],
+                      ["Brabus","Porsche","Aston Martin","Bentley","Maybach","BMW","Ford","Audi"]],
+                    [["Popular Models"],
+                    ["Ferrari LaFerrari","Bugatti Chiron","Ferrari F40","Lamborghini Aventador","McLaren P1","Porsche 918","Ford GT","Mercedes SLR McLaren"],
+                    ["Pagani Huayra","Porsche Carrera GT","Lamborghini Urus","Lamborghini Murcielago","Ferrari Enzo","Lexus LFA","Brabus G-Class","Rolls-Royce Cullinan"]
+                  ],]
+  const li4_items =[[["Popular Manufacturer"],["Embraer","Dassault","Gulfstream","Bombardier","Cessna","Boeing","Pilatus"],],]
+
+  const [li_content, set_li_content] = useState(li_default);
 
 
-  const handleChange =() =>{
-    if(window.scrollY>=50){
+  const handleChange = () => {
+    if (window.scrollY >= 50) {
       setColor(true)
     }
     else setColor(false)
   }
 
-  useEffect(()=>{
-    window.addEventListener('scroll',handleChange)
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleChange)
   })
 
 
@@ -40,165 +72,120 @@ function Navbar1() {
     <>
       {["lg"].map((expand) => (
         <div>
-        <Navbar key={expand}  expand={expand} className={color ? 'mb-3 navbar nav-bg' : 'mb-3 navbar nav'}>
-        <script></script>
-          <Container fluid>
-            <div className="left-nav">
-              <Button
-                variant="primary"
-                onClick={handleShow}
-                className="me-2 nav-btn"
-              >
-                <span className="material-symbols-outlined hamburger-menu">menu</span>
-              </Button>
-              <h1 style={{ fontSize: "1.5rem", fontWeight: "bold" }} className={play.className}>ShauKeens</h1>
-            </div>
-            <div className="all-nav-btns">
-              <Button className="me-2 nav-btn">
-                <span className="nav-btns">Just For You</span>
-              </Button>
-              <Button className="me-2 nav-btn">
-                <span className="nav-btns">List With Us</span>
-              </Button>
-              <Button className="me-2 nav-btn sign-in-btn">
-                <span className="material-symbols-outlined">person</span>
-                <span id="signin">Login</span>
-              </Button>
-            </div>
-            <Offcanvas show={show} onHide={handleClose}>
-              <Offcanvas.Header>
-                <Offcanvas.Title className={play.className}>
-                  Shaukeens
-                </Offcanvas.Title>
+          <Navbar key={expand} expand={expand} className={color ? 'mb-3 navbar nav-bg' : 'mb-3 navbar nav'}>
+            <script></script>
+            <Container fluid>
+              <div className="left-nav">
                 <Button
                   variant="primary"
-                  onClick={handleClose}
-                  className="me-2 nav-close-btn"
+                  onClick={handleShow}
+                  className="me-2 nav-btn"
                 >
-                  <span class="material-symbols-outlined">close</span>
+                  <span className="material-symbols-outlined hamburger-menu">menu</span>
                 </Button>
-              </Offcanvas.Header>
-              <Offcanvas.Body>
-                <hr />
-                <div className="btn-div">
-                  <div>
-                    <span class="material-symbols-outlined">
-                      account_circle
-                    </span>
-                    <Button>Sign in / Register</Button>
-                  </div>
-                  <div>
-                    <span class="material-symbols-outlined">recommend</span>
-                    <Button>Just For You</Button>
-                  </div>
-                </div>
-                <hr />
-                <div className="btn-div">
-                  <div>
-                    <span class="material-symbols-outlined">
-                      directions_boat
-                    </span>
-                    <Button>Yachts</Button>
-                  </div>
-                  <div>
-                    <span class="material-symbols-outlined">villa</span>
-                    <Button>Real Estates</Button>
-                  </div>
-                  <div>
-                    <span class="material-symbols-outlined">helicopter</span>
-                    <Button>Helicopters</Button>
-                  </div>
-                  <div>
-                    <span class="material-symbols-outlined">
-                      flight_takeoff
-                    </span>
-                    <Button>Jets</Button>
-                  </div>
-                  <div>
-                    <span class="material-symbols-outlined">two_wheeler</span>
-                    <Button>Bikes</Button>
-                  </div>
-                  <div>
-                    <span class="material-symbols-outlined">
-                      directions_car
-                    </span>
-                    <Button>Cars</Button>
-                  </div>
-                </div>
-                <hr />
-                <div className="btn-div-2">
-                  <Button>List With Us</Button>
-                  <Button>Help & FAQ</Button>
-                  <Button>About</Button>
-                  <Button>Contact</Button>
-                </div>
-                <hr />
-              </Offcanvas.Body>
-            </Offcanvas>
-          </Container>
-        </Navbar>
-        <div>
-            <ul className="flex bg-transparent -mt-4 space-x-7 p-2 border-t-1 border-b-1 text-white responsive_nav2">
-                <li onMouseEnter={()=>
-                    {document.getElementsByClassName("li_1_div")[0].style.visibility="visible"}}
-                    className="left_li1 pointer">Yachts</li>
-                <li onMouseEnter={()=>
-                    {document.getElementsByClassName("li_1_div")[0].style.visibility='visible'}}
-                    className="pointer">Real Estates</li>
-                <li onMouseEnter={()=>
-                    {document.getElementsByClassName("li_1_div")[0].style.visibility="visible"}}
-                    className="pointer">Helicopter</li>
-                <li onMouseEnter={()=>
-                    {document.getElementsByClassName("li_1_div")[0].style.visibility="visible"}}
-                    className="pointer">Jets</li>
-                <li onMouseEnter={()=>
-                    {document.getElementsByClassName("li_1_div")[0].style.visibility="visible"}}
-                    className="pointer">Bikes</li>
-                <li onMouseEnter={()=>
-                    {document.getElementsByClassName("li_1_div")[0].style.visibility="visible"}}
-                    className="pointer">Cars</li>
-            </ul>
-            <div onMouseLeave={()=>
-                {console.log(document.getElementsByClassName("li_1_div")[0].style.visibility="hidden")}}
-                className="bg-white -mt-4 pl-10 pt-4 li_1_div ">
-                <h6 className="ml-8">Popular Builders</h6>
-                <div className="li_1">
-                    <div>
-                        <ul>
-                            <li>Sunseeker</li>
-                            <li>Azimut</li>
-                            <li>Riva</li>
-                            <li>Benetti</li>
-                            <li>Custom</li>
-                            <li>Cranchi</li>
-                            <li>Beneteau</li>
-                            <li>Ferretti</li>
-                        </ul>
-                    </div>
-                    <div>
-                        <ul>
-                            <li>Sea Ray</li>
-                            <li>Sunreef</li>
-                            <li>Chris Craft</li>
-                            <li>Heesen</li>
-                            <li>Pershing</li>
-                            <li>Hatteras</li>
-                            <li>Princess</li>
-                            <li>Viking</li>
-                        </ul>
-                    </div>
-                </div>
-                <div className="flex flex-end justify-end text-center">
-                    <button className="bg-black p-2 text-neutral-300 ">View all yachts
-
-                    </button>
-                    <div className="bg-black btn-top"><svg xmlns="http://www.w3.org/2000/svg" height="18"
-                            viewBox="0 96 960 960" fill="white" width="48">
-                            <path d="m561 814-43-42 168-168H160v-60h526L517 375l43-42 241 241-240 240Z" />
-                        </svg></div>
-                </div>
-            </div>
+                <h1 style={{ fontSize: "1.5rem", fontWeight: "bold" }} className={play.className}>ShauKeens</h1>
               </div>
+              <div className="all-nav-btns">
+                <Button className="me-2 nav-btn">
+                  <span className="nav-btns">Just For You</span>
+                </Button>
+                <Button className="me-2 nav-btn">
+                  <span className="nav-btns">List With Us</span>
+                </Button>
+                <Button className="me-2 nav-btn sign-in-btn">
+                  <span className="material-symbols-outlined">person</span>
+                  <span id="signin">Login</span>
+                </Button>
+              </div>
+              <Offcanvas show={show} onHide={handleClose}>
+                <Offcanvas.Header>
+                  <Offcanvas.Title className={play.className}>
+                    Shaukeens
+                  </Offcanvas.Title>
+                  <Button
+                    variant="primary"
+                    onClick={handleClose}
+                    className="me-2 nav-close-btn"
+                  >
+                    <span class="material-symbols-outlined">close</span>
+                  </Button>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                  <hr />
+                  <div className="btn-div">
+                    <div>
+                      <span class="material-symbols-outlined">
+                        account_circle
+                      </span>
+                      <Button>Sign in / Register</Button>
+                    </div>
+                    <div>
+                      <span class="material-symbols-outlined">recommend</span>
+                      <Button>Just For You</Button>
+                    </div>
+                  </div>
+                  <hr />
+                  <div className="btn-div">
+                    <div>
+                      <span class="material-symbols-outlined">
+                        directions_boat
+                      </span>
+                      <Button>Yachts</Button>
+                    </div>
+                    <div>
+                      <span class="material-symbols-outlined">villa</span>
+                      <Button>Real Estates</Button>
+                    </div>
+                    <div>
+                      <span class="material-symbols-outlined">helicopter</span>
+                      <Button>Helicopters</Button>
+                    </div>
+                    <div>
+                      <span class="material-symbols-outlined">
+                        flight_takeoff
+                      </span>
+                      <Button>Jets</Button>
+                    </div>
+                    <div>
+                      <span class="material-symbols-outlined">two_wheeler</span>
+                      <Button>Bikes</Button>
+                    </div>
+                    <div>
+                      <span class="material-symbols-outlined">
+                        directions_car
+                      </span>
+                      <Button>Cars</Button>
+                    </div>
+                  </div>
+                  <hr />
+                  <div className="btn-div-2">
+                    <Button>List With Us</Button>
+                    <Button>Help & FAQ</Button>
+                    <Button>About</Button>
+                    <Button>Contact</Button>
+                  </div>
+                  <hr />
+                </Offcanvas.Body>
+              </Offcanvas>
+            </Container>
+          </Navbar>
+          <div>
+            <ul className="flex bg-transparent -mt-4 space-x-7 p-2 border-t-1 border-b-1 text-white responsive_nav2">
+              <li onMouseEnter={() => { document.getElementsByClassName("li_1_div")[0].style.visibility = "visible"; set_li_content(li1_items) ; setdefaultItem("Yachts") }}
+                className="left_li1 pointer">Yachts</li>
+              <li onMouseEnter={() => { document.getElementsByClassName("li_1_div")[0].style.visibility = "visible"; set_li_content(li2_items) ; setdefaultItem("Real Estates") }}
+                className="pointer">Real Estates</li>
+              <li onMouseEnter={() => { document.getElementsByClassName("li_1_div")[0].style.visibility = "visible" ; set_li_content(li_default); setdefaultItem("Helicopter") }}
+                className="pointer">Helicopter</li>
+              <li onMouseEnter={() => { document.getElementsByClassName("li_1_div")[0].style.visibility = "visible" ;set_li_content(li4_items); setdefaultItem("Jets") }}
+                className="pointer">Jets</li>
+              <li onMouseEnter={() => { document.getElementsByClassName("li_1_div")[0].style.visibility = "visible" ; set_li_content(li_default); setdefaultItem("Bikes") }}
+                className="pointer">Bikes</li>
+              <li onMouseEnter={() => { document.getElementsByClassName("li_1_div")[0].style.visibility = "visible" ;set_li_content(li6_items); setdefaultItem("Cars") }}
+                className="pointer">Cars</li>
+            </ul>
+            <Nav_li_items props={li_content} item={default_item}/>
+          </div>
         </div>
       ))}
     </>
