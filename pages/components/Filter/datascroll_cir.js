a2c896bf9e0052193ca382b068f6b9632ed027a1
Harsh Kanojia <96467990+Eyesnatcher-coder@users.noreply.github.com>
2023-05-01 00:31:05 +0530

    filter frontend completed

--- pages/components/Filter/datascroll_cir.js
+++ pages/components/Filter/datascroll_cir.js
@@ -5,7 +5,7 @@ export default function Datascroll_R({data}){
         <div>
             <div className="">{
                 data.map((item)=>{
-                    return<div><input className="p-1 mx-3" type="radio" id={item} name={item} value={item} />
+                    return<div><input className="p-1 mx-3 scale-125" type="radio" id={item} name={item} value={item} />
                     <label className="font-mono font-semibold" for="vehicle3">{item}</label>
                     </div>
                 })
--- pages/components/Filter/datascroll_sq.js
+++ pages/components/Filter/datascroll_sq.js
@@ -5,7 +5,7 @@ export default function Datascroll({data}){
         <div>
             <div className="">{
                 data.map((item)=>{
-                    return<div><input className="p-1 mx-3"  type="checkbox" id={item} name={item} value={item} />
+                    return<div><input className="p-1 mx-3 scale-125"  type="checkbox" id={item} name={item} value={item} />
                     <label className="font-mono" for="vehicle3">{item}</label>
                     </div>
                 })
