0286075adad8b7e925954e2085c0537e479475eb
Ashish Bhardwaj <101322407+ashish5433@users.noreply.github.com>
2026-05-06 06:27:18 +0530

    Update Firebase configuration parameters

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -8,12 +8,12 @@ import {getAuth,GoogleAuthProvider} from 'firebase/auth'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDVp3NNK6dRzox6CbH7Lkn1asW8kxxXOVE",
-  authDomain: "imageupload-83d5d.firebaseapp.com",
-  projectId: "imageupload-83d5d",
-  storageBucket: "imageupload-83d5d.appspot.com",
-  messagingSenderId: "697179914318",
-  appId: "1:697179914318:web:715fdfeea344a9e9f8031f"
+  apiKey: "AIzaSyDVp3NNox6CbH7Lkn1asW8kxxXOVE",
+  authDomain: "imageupload.firebaseapp.com",
+  projectId: "imageupload",
+  storageBucket: "imageupl.appspot.com",
+  messagingSenderId: "",
+  appId: "1:697179"
 };
 
 
